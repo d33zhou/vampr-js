036fac621ab3986d03017f1e24fe35fddad36782
d33zhou <d33zhou@outlook.com>
2021-12-10 21:55:52 +0000

    update for intro exercises

--- vampire.js
+++ vampire.js
@@ -10,22 +10,35 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let number = 0;
+    let currentVamp = this;
 
+    while(currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      number++;
+    }
+
+    return number;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -36,9 +49,29 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let vamp1 = this;
+    let vamp2 = vampire;
 
+    while (vamp1.creator && vamp2.creator) {
+      let vamp1Tier = vamp1.numberOfVampiresFromOriginal;
+      let vamp2Tier = vamp2.numberOfVampiresFromOriginal;
+
+      if (vamp1Tier === vamp2Tier) {
+        if (vamp1.name === vamp2.name) {
+          return vamp1;
+        } else {
+          vamp1 = vamp1.creator;
+          vamp2 = vamp2.creator;
+        }
+      } else if (vamp1Tier > vamp2Tier) {
+        vamp1 = vamp1.creator;
+      } else {
+        vamp2 = vamp2.creator;
+      }
+    }
+
+    return !vamp1.creator ? vamp1 : vamp2;
   }
 }
 
 module.exports = Vampire;
-
